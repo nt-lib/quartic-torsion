d3e81686a85b1bb1e2c61f73b1345d0d44b37e7a
Maarten Derickx <maarten@mderickx.nl>
2024-12-06 14:10:12 +0100

    Use the newly created hecke filter function

--- X1_25.m
+++ X1_25.m
@@ -20,14 +20,7 @@ print "Claim 1 successfully verified";
 q := 7;
 
 // Claim 2: Cannot have reduction of the form deg4
-todo4 := [];
-for D in NoncuspidalPlaces(X,4) do
-  A_qD := HeckeOperator(X,q,D)-q*DiamondOperator(X,q,D)-D;
-  if IsPrincipal(A_qD) then
-    Append(~todo4, D);
-  end if;
-end for;
-assert #todo4 eq 0;
+assert #HeckeFilter(X, q, NoncuspidalPlaces(X,4)) eq 0;
 print "Claim 2 successfully verified";
 
 exit;
--- X1_32.m
+++ X1_32.m
@@ -19,25 +19,11 @@ print "Claim 1 successfully verified";
 q := 5;
 
 // Claim 2: Cannot have reduction of the form deg3 + cusp
-todo3 := [];
-for D in NoncuspidalPlaces(X,3) do
-  A_qD := HeckeOperator(X,q,D)-q*DiamondOperator(X,q,D)-D;
-  if IsPrincipal(A_qD) then
-    Append(~todo3, D);
-  end if;
-end for;
-assert #todo3 eq 0;
+assert #HeckeFilter(X, q, NoncuspidalPlaces(X,3)) eq 0;
 print "Claim 2 successfully verified";
 
 // Claim 3: Cannot have reduction of the form deg4
-todo4 := [];
-for D in NoncuspidalPlaces(X,4) do
-  A_qD := HeckeOperator(X,q,D)-q*DiamondOperator(X,q,D)-D;
-  if IsPrincipal(A_qD) then
-    Append(~todo4, D);
-  end if;
-end for;
-assert #todo4 eq 0;
+assert #HeckeFilter(X, q, NoncuspidalPlaces(X,4)) eq 0;
 print "Claim 3 successfully verified";
 
 exit;
