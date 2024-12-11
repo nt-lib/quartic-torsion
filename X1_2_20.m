d052ded698e6f2f426cb311c4cc93745360e5e96
Maarten Derickx <maarten@mderickx.nl>
2024-12-11 13:59:26 +0100

    Verify that cusps lift for X1_2_20

--- X1_2_20.m
+++ X1_2_20.m
@@ -11,7 +11,7 @@ AttachSpec("mdmagma/v2/mdmagma.spec");
 
 X := MDX11(2,20,GF(7));
 
-// Claim 1 compute the number of noncuspidal places of degree <= 4
+// Claim 1: compute the number of noncuspidal places of degree <= 4
 assert [#NoncuspidalPlaces(X,i) : i in [1..4]] eq [0, 16, 128, 704];                                                                                                                            
 print "Claim 1 successfully verified";
 
@@ -21,27 +21,35 @@ print "Claim 1 successfully verified";
 // deg3 + cusp
 // deg4
 
+// Claim 2: verify that all frobenius oribits of cusps lift to Q
+// We do this by verifying that the cusp degrees over F_7 match those over Q listed on
+// https://beta.lmfdb.org/ModularCurve/Q/20.576.9-20.b.4.2/
+cusp_degrees := Sort([Degree(c) : c in Cusps(X)]);
+assert cusp_degrees eq [ 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 4, 4, 4, 4 ];
+print "Claim 2 successfully verified";
+
+
 // The smallest values of q we can use such that we know
 // T_q-q<q>-1 vanishes on Q rational torsion.
 q := 3;
 
-// Claim 2: Cannot have reduction of the form deg2 + 2 cusps
+// Claim 3: Cannot have reduction of the form deg2 + 2 cusps
 assert #HeckeSieve(X, q, NoncuspidalPlaces(X,2)) eq 0;
-print "Claim 2 successfully verified";
-
-
-// Claim 3: Cannot have reduction of the form deg2 + deg2
-S := [x+y: x,y in NoncuspidalPlaces(X,2)];
-assert #HeckeSieve(X, q, S) eq 0;
 print "Claim 3 successfully verified";
 
 
-// Claim 4: Cannot have reduction of the form deg3 + cusp
-assert #HeckeSieve(X, q, NoncuspidalPlaces(X,3)) eq 0;
+// Claim 4: Cannot have reduction of the form deg2 + deg2
+S := [x+y: x,y in NoncuspidalPlaces(X,2)];
+assert #HeckeSieve(X, q, S) eq 0;
 print "Claim 4 successfully verified";
 
-// Claim 5: Cannot have reduction of the form deg4
-assert #HeckeSieve(X, q, NoncuspidalPlaces(X,4)) eq 0;
+
+// Claim 5: Cannot have reduction of the form deg3 + cusp
+assert #HeckeSieve(X, q, NoncuspidalPlaces(X,3)) eq 0;
 print "Claim 5 successfully verified";
+
+// Claim 6: Cannot have reduction of the form deg4
+assert #HeckeSieve(X, q, NoncuspidalPlaces(X,4)) eq 0;
+print "Claim 6 successfully verified";
 
 exit;
