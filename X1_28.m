d04e994314feed822cfde18d3a526f09d053416e
Maarten Derickx <maarten@mderickx.nl>
2024-12-16 13:58:49 +0100

    Fix x1_28 after the cusp lift bug

--- X1_28.m
+++ X1_28.m
@@ -18,26 +18,24 @@ print "Claim 1 successfully verified";
 // deg3 + cusp
 // deg4
 
-// The smallest values of q we can use
-q := 3;
 
 // Claim 2: Cannot have reduction of the form deg2 + 2 cusps
-assert #HeckeSieve(X, q, NoncuspidalPlaces(X,2)) eq 0;
+assert #HeckeSieve(X, 13, NoncuspidalPlaces(X,2)) eq 0;
 print "Claim 2 successfully verified";
 
 
 // Claim 3: Cannot have reduction of the form deg2 + deg2
 S := [x+y: x,y in NoncuspidalPlaces(X,2)];
-assert #HeckeSieve(X, q, S) eq 0;
+assert #HeckeSieve(X, 3, S) eq 0;
 print "Claim 3 successfully verified";
 
 
 // Claim 4: Cannot have reduction of the form deg3 + cusp
-assert #HeckeSieve(X, q, NoncuspidalPlaces(X,3)) eq 0;
+assert #HeckeSieve(X, 13, NoncuspidalPlaces(X,3)) eq 0;
 print "Claim 4 successfully verified";
 
 // Claim 5: Cannot have reduction of the form deg4
-assert #HeckeSieve(X, q, NoncuspidalPlaces(X,4)) eq 0;
+assert #HeckeSieve(X, 3, NoncuspidalPlaces(X,4)) eq 0;
 print "Claim 5 successfully verified";
 
 exit;
