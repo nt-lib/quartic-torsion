2a60eb0b9fadca4bf7d9ce59ca399d0e8ba15d42
Maarten Derickx <maarten@mderickx.nl>
2024-12-17 13:20:10 +0100

    Also automatically verify the congruence checks

--- verify_all.sh
+++ verify_all.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ls X1_*.m | parallel --joblog logs/verify_joblog.txt -j ${1:-10} 'magma {} >| logs/{.}.txt'
+ls *.m | parallel --joblog logs/verify_joblog.txt -j ${1:-10} 'magma {} >| logs/{.}.txt'
